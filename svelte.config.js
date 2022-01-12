baee0e59d813430e8e83cb060b3c16db9017382d
Bernhard Mühl <bernhard.muehl@vbcf.ac.at>
2022-01-12 11:04:30 +0100

    Fix github pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,14 @@ const config = {
   preprocess: [preprocess({})],
 
   kit: {
-    adapter: adapter(),
+    paths: {
+      base: '/airofjasmine'
+    },
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
